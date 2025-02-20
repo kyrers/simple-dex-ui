d1974ac4d6768e7d27846521d8af77ba52ebf6b3
kyrers <kyrers.eth@gmail.com>
2025-02-20 18:36:11 +0000

    Initial UI structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,19 @@
 "use client";
-
-import useTokenA from "@/hooks/useTokenA";
-import useTokenB from "@/hooks/useTokenB";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
+import { ConnectButtonWrapper, PageWrapper } from "./styles";
+import TokenInfo from "@/components/tokenInfo/tokenInfo";
 
 export default function Home() {
   const { isConnected } = useAccount();
-  const { balance: tokenABalance } = useTokenA();
-  const { balance: tokenBBalance } = useTokenB();
 
   return (
-    <div>
-      <ConnectButton />
-      {!isConnected ? (
-        <h2>Connect wallet</h2>
-      ) : (
-        <>
-          <p>Token A Balance: {tokenABalance}</p>
-          <p>Token B Balance: {tokenBBalance}</p>
-        </>
-      )}
-    </div>
+    <PageWrapper>
+      <ConnectButtonWrapper>
+        <ConnectButton />
+      </ConnectButtonWrapper>
+
+      {!isConnected ? <h2>Connect wallet</h2> : <TokenInfo />}
+    </PageWrapper>
   );
 }
---
+++ src/app/styles.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  padding: 40px;
+`;
+
+export const ConnectButtonWrapper = styled.div`
+  align-self: end;
+`;
